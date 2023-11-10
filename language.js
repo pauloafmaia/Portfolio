6a40e824391bcc49fafb60f7c26e66c29bbc80a0
Paulo Maia <pauloafmaia9@gmail.com>
2023-11-09 21:37:09 -0300

    updates

--- language.js
+++ language.js
@@ -29,6 +29,7 @@ $(function () {
     );
     $(".language-selected").text("pt-BR");
     $(".language-selected").removeClass("change-en");
+    $(".language-selected").removeClass("change-es");
     $(".language-selected").addClass("change-br");
   });
 });
@@ -62,6 +63,41 @@ $(function () {
     );
     $(".language-selected").text("en-US");
     $(".language-selected").removeClass("change-br");
+    $(".language-selected").removeClass("change-es");
     $(".language-selected").addClass("change-en");
+  });
+});
+
+$(function () {
+  $(".es").click(function () {
+    $(".navbar").children().eq(0).text("Comenzar");
+    $(".navbar").children().eq(1).text("Acerca de mí");
+    $(".navbar").children().eq(2).text("Proyectos");
+    $(".navbar").children().eq(3).text("Habilidades");
+    $(".navbar").children().eq(4).text("Contactos");
+    $("#text-home").text("Desarrollador de Software");
+    $("#text-sobre-1").text(
+      "Developing interfaces and APIs with Java, Javascript, React, HTML and CSS since 2022"
+    );
+    $("#text-sobre-2").text(
+      "Studying Systems Analysis and Development at UNINASSAU"
+    );
+    $("#text-sobre-3").text("Interest in Front-end and Back-end development");
+    $("#text-sobre-4").text(
+      "Looking for an opportunity to work as a Developer"
+    );
+    $("#text-projeto-1").text(
+      "API created for adding, editing and removing films from the Star Wars film series. Carried out with Back-end in NodeJs and Front-end with ReactJs and Tailwind."
+    );
+    $("#text-projeto-2").text(
+      "Website created for wedding. Made with ReactJs and Tailwind"
+    );
+    $("#text-projeto-3").text(
+      "Website created for wedding. Created with HTML and CSS"
+    );
+    $(".language-selected").text("es-ES");
+    $(".language-selected").removeClass("change-br");
+    $(".language-selected").removeClass("change-en");
+    $(".language-selected").addClass("change-es");
   });
 });

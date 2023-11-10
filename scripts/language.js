0f40bd5c05519a61e0df2c92aea2cfd28193756b
Paulo Maia <pauloafmaia9@gmail.com>
2023-11-10 12:21:11 -0300

    updates

--- scripts/language.js
+++ scripts/language.js
@@ -6,6 +6,10 @@ $(function () {
     $(".navbar").children().eq(3).text("Habilidades");
     $(".navbar").children().eq(4).text("Contatos");
     $("#text-home").text("Desenvolvedor de Software");
+    $("#titulo-sobre").text("SOBRE MIM");
+    $("#titulo-projetos").text("PROJETOS");
+    $("#titulo-habilidades").text("HABILIDADES");
+    $("#titulo-contatos").text("CONTATOS");
     $("#text-sobre-1").text(
       "Desenvolvendo interfaces e APIs com Java, Javascript, React, HTML e CSS desde 2022"
     );
@@ -27,9 +31,14 @@ $(function () {
     $("#text-projeto-3").text(
       "Site criado para casamento. Realizado com HTML e CSS"
     );
+    $("#text-habilidades-1").text(
+      "Desenvolvimento de sites responsivos e interativos utilizando tecnologias como ReactJs, com bibliotecas populares como Tailwind CSS, HTML e CSS"
+    );
+    $("#text-habilidades-2").text(
+      "Desenvolvimento de APIs com as linguagens NodeJs e Java com Spring Boot."
+    );
     $(".language-selected").text("pt-BR");
     $(".language-selected").removeClass("change-en");
-    $(".language-selected").removeClass("change-es");
     $(".language-selected").addClass("change-br");
   });
 });
@@ -39,9 +48,13 @@ $(function () {
     $(".navbar").children().eq(0).text("Home");
     $(".navbar").children().eq(1).text("About me");
     $(".navbar").children().eq(2).text("Projects");
-    $(".navbar").children().eq(3).text("Habilities");
+    $(".navbar").children().eq(3).text("Skills");
     $(".navbar").children().eq(4).text("Contact");
     $("#text-home").text("Software Developer");
+    $("#titulo-sobre").text("ABOUT ME");
+    $("#titulo-projetos").text("PROJECTS");
+    $("#titulo-habilidades").text("SKILLS");
+    $("#titulo-contatos").text("CONTACTS");
     $("#text-sobre-1").text(
       "Developing interfaces and APIs with Java, Javascript, React, HTML and CSS since 2022"
     );
@@ -60,44 +73,15 @@ $(function () {
     );
     $("#text-projeto-3").text(
       "Website created for wedding. Created with HTML and CSS"
+    );
+    $("#text-habilidades-1").text(
+      "Development of responsive and interactive websites using technologies such as ReactJs, with popular libraries such as Tailwind CSS, HTML and CSS"
+    );
+    $("#text-habilidades-2").text(
+      "Development of APIs with NodeJs and Java with Spring boot"
     );
     $(".language-selected").text("en-US");
     $(".language-selected").removeClass("change-br");
-    $(".language-selected").removeClass("change-es");
     $(".language-selected").addClass("change-en");
-  });
-});
-
-$(function () {
-  $(".es").click(function () {
-    $(".navbar").children().eq(0).text("Comenzar");
-    $(".navbar").children().eq(1).text("Acerca de mí");
-    $(".navbar").children().eq(2).text("Proyectos");
-    $(".navbar").children().eq(3).text("Habilidades");
-    $(".navbar").children().eq(4).text("Contactos");
-    $("#text-home").text("Desarrollador de Software");
-    $("#text-sobre-1").text(
-      "Developing interfaces and APIs with Java, Javascript, React, HTML and CSS since 2022"
-    );
-    $("#text-sobre-2").text(
-      "Studying Systems Analysis and Development at UNINASSAU"
-    );
-    $("#text-sobre-3").text("Interest in Front-end and Back-end development");
-    $("#text-sobre-4").text(
-      "Looking for an opportunity to work as a Developer"
-    );
-    $("#text-projeto-1").text(
-      "API created for adding, editing and removing films from the Star Wars film series. Carried out with Back-end in NodeJs and Front-end with ReactJs and Tailwind."
-    );
-    $("#text-projeto-2").text(
-      "Website created for wedding. Made with ReactJs and Tailwind"
-    );
-    $("#text-projeto-3").text(
-      "Website created for wedding. Created with HTML and CSS"
-    );
-    $(".language-selected").text("es-ES");
-    $(".language-selected").removeClass("change-br");
-    $(".language-selected").removeClass("change-en");
-    $(".language-selected").addClass("change-es");
   });
 });
